58539a8e0642ea75cdd8263a15224c4127f18b61
Vigi73 <vigi73@mail.ru>
2022-08-23 12:50:07 +0700

    22222

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import qrcode
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'] )
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', image='logo.jpg')
 
@@ -28,5 +28,6 @@ def get_code():
 
     return render_template('index.html', image='qr.png')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
