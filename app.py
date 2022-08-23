66f7981ba84a593e7fa1c5a74aa61fd669b8d470
Vigi73 <vigi73@mail.ru>
2022-08-23 10:53:23 +0700

    1111

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, url_for, redirect, render_template, request
+import qrcode
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'] )
+def index():
+    return render_template('index.html', image='logo.jpg')
+
+
+@app.route('/qr', methods=['GET'])
+def qr():
+    return render_template('qr.html')
+
+
+@app.route('/get_code', methods=['POST'])
+def get_code():
+    fio = request.form['fio']
+    phone = request.form['phone']
+    email = request.form['email']
+
+    text = f'👤 {fio}\n☎ {phone}\n📧  {email}\n'
+
+    img = qrcode.make(text)
+    img.save('static/images/qr.png')
+
+    return render_template('index.html', image='qr.png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
